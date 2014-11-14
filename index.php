7d766c005d49fb7a0f1e544a0cb5ef64859cadcf
sokhavisal <sokha.visal@gmail.com>
2014-11-14 08:25:34 +0700

    visal  2014/09/26 satellite

---
+++ index.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="css/css.css">
+	<script src="js/js.js"></script>
+        <title>Mywebsite</title>
+    </head>
+    <body>
+	<div id="nav">
+	    <div id="nav-wrapper">
+		<ul>
+		    <li><a href="#">Home</a>
+		    <li><a href="#">About us</a>
+		    <li><a href="#">Contact</a>
+			 <li><a href="#">Home</a>
+		    <li><a href="#">About us</a>
+		    <li><a href="#">Contact</a>
+			 <li><a href="#">Home</a>
+		    <li><a href="#">About us</a>
+		    <li><a href="#">Contact</a>
+		</ul>
+	    </div>   
+	</div>
+	
+	
+	
+	
+	
+	
+	
+<div id ="wrapper">
+	    <div id="leftside" >
+	    <h3>atest</h3>
+		<p>helloworld</p>
+		<h3>atest</h3>
+		<p>helloworld</p>
+		<h3>atest</h3>
+		<p>helloworld</p>
+		<h3>atest</h3>
+		<p>helloworld</p>
+		<h3>atest</h3>
+		<p>helloworld</p>
+	    </div>
+	    <div id ="content">
+		<h3>atest</h3>
+		<p>helloworld</p>
+		<h3>atest</h3>
+		<p>helloworld</p>
+		<h3>atest</h3>
+		<p>helloworld</p>
+		<h3>atest</h3>
+		<p>helloworld</p>
+		<h3>atest</h3>
+		<p>helloworld</p>
+		<h3>atest</h3>
+		<p>helloworld</p>
+		<h3>atest</h3>
+		<p>helloworld</p>
+		<h3>atest</h3>
+		<p>helloworld</p>
+		<h3>atest</h3>
+		<p>helloworld</p>
+		<h3>atest</h3>
+		<p>helloworld</p>
+	   
+ 	    </div>
+	    <div id="rightside" >
+		<h3>atest</h3>
+		<p>helloworld</p>
+		<h3>atest</h3>
+		<p>helloworld</p>
+		<h3>atest</h3>
+		<p>helloworld</p>
+		<h3>atest</h3>
+		<p>helloworld</p>
+		<h3>atest</h3>
+		<p>helloworld</p>
+	    </div>
+    
+    
+    <div id="footer">
+		<div id="footerMenu">
+		    <div id="footer-nav">
+			<ul>
+			    <li><a href="#">Home</a>
+			    <li><a href="#">About us</a>
+			    <li><a href="#">Contact</a>
+			    <li><a href="#">Home</a>
+			    <li><a href="#">About us</a>
+			    <li><a href="#">Contact</a>
+			    <li><a href="#">Home</a>
+			    <li><a href="#">About us</a>
+			    <li><a href="#">Contact</a>
+			</ul>
+		    </div>
+		</div>
+		<div id="copy"><span>Copyright &copy; Khmer Kon Kat.,Ltd. All Rights Reserved.</span></div>
+    </div>
+	
+	   
+</div>
+	
+
+	
+    
+    </body>
+</html>
