fbdb93caca30d6f318466ffbb74472abf2ed2135
sokhavisal <sokha.visal@gmail.com>
2014-11-14 16:31:02 +0700

    visal  2014/09/26 satellite

--- index.php
+++ index.php
@@ -9,6 +9,9 @@ and open the template in the editor.
         <meta charset="UTF-8">
 	<link rel="stylesheet" type="text/css" href="css/css.css">
 	<script src="js/js.js"></script>
+	<link rel="stylesheet" href="css/styles.css">
+	<script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+	<script src="js/script.js"></script>
         <title>Mywebsite</title>
     </head>
     <body>
@@ -24,78 +27,154 @@ and open the template in the editor.
 			 <li><a href="#">Home</a>
 		    <li><a href="#">About us</a>
 		    <li><a href="#">Contact</a>
+			
 		</ul>
-	    </div>   
+		
+	    </div>
+	   
+	    
+	    
 	</div>
-	
-	
-	
-	
-	
-	
+	<div style="clear: both;"></div>	
 	
 <div id ="wrapper">
-	    <div id="leftside" >
-	    <h3>atest</h3>
-		<p>helloworld</p>
-		<h3>atest</h3>
-		<p>helloworld</p>
-		<h3>atest</h3>
-		<p>helloworld</p>
-		<h3>atest</h3>
-		<p>helloworld</p>
-		<h3>atest</h3>
-		<p>helloworld</p>
+	
+	<div id="leftside" > <!-- block leftSide -->
+	   <div id='cssmenu'>
+		<ul>
+		   <li class='active'><a href='#'><span>Category<img src="img/acc1.png" width="40px" height="30px" style=" float: right; "></span></a></li>
+		   <li class='has-sub'><a href='#'><span>Products</span></a>
+		      <ul>
+			 <li><a href='#'><span>Product 1</span></a></li>
+			 <li><a href='#'><span>Product 2</span></a></li>
+			 <li class='last'><a href='#'><span>Product 3</span></a></li>
+		      </ul>
+		   </li>
+		   <li class='has-sub'><a href='#'><span>About</span></a>
+		      <ul>
+			 <li><a href='#'><span>Company</span></a></li>
+			 <li class='last'><a href='#'><span>Contact</span></a></li>
+		      </ul>
+		   </li>
+		    <li class='has-sub'><a href='#'><span>Products</span></a>
+		      <ul>
+			 <li><a href='#'><span>Product 1</span></a></li>
+			 <li><a href='#'><span>Product 2</span></a></li>
+			 <li class='last'><a href='#'><span>Product 3</span></a></li>
+		      </ul>
+		   </li>
+		   <li class='has-sub'><a href='#'><span>About</span></a>
+		      <ul>
+			 <li><a href='#'><span>Company</span></a></li>
+			 <li class='last'><a href='#'><span>Contact</span></a></li>
+		      </ul>
+		   </li>
+		   
+<!--		   ///<li class='last'><a href='#'><span>Contact</span></a></li>-->
+		</ul>
 	    </div>
-	    <div id ="content">
-		<h3>atest</h3>
-		<p>helloworld</p>
-		<h3>atest</h3>
-		<p>helloworld</p>
-		<h3>atest</h3>
-		<p>helloworld</p>
-		<h3>atest</h3>
-		<p>helloworld</p>
-		<h3>atest</h3>
-		<p>helloworld</p>
-		<h3>atest</h3>
-		<p>helloworld</p>
-		<h3>atest</h3>
-		<p>helloworld</p>
-		<h3>atest</h3>
-		<p>helloworld</p>
-		<h3>atest</h3>
-		<p>helloworld</p>
-		<h3>atest</h3>
-		<p>helloworld</p>
-	   
- 	    </div>
-	    <div id="rightside" >
-		<h3>atest</h3>
-		<p>helloworld</p>
-		<h3>atest</h3>
-		<p>helloworld</p>
-		<h3>atest</h3>
-		<p>helloworld</p>
-		<h3>atest</h3>
-		<p>helloworld</p>
-		<h3>atest</h3>
-		<p>helloworld</p>
+	    <br>
+	 
+	 <!-- Accesory Category Item. -->
+	    <div id='cssmenu'>
+		<ul>
+		   <li class='active'><a href='#'><span>Accesories<img src="img/sevices.png" width="35px" height="30px" style=" float: right; "></span></a></li>
+		   <li class='has-sub'><a href='#'><span>Products </span></a>
+		      <ul>
+			 <li><a href='#'><span>Product 1</span></a></li>
+			 <li><a href='#'><span>Product 2</span></a></li>
+			 <li class='last'><a href='#'><span>Product 3</span></a></li>
+		      </ul>
+		   </li>
+		   <li class='has-sub'><a href='#'><span>About</span></a>
+		      <ul>
+			 <li><a href='#'><span>Company</span></a></li>
+			 <li class='last'><a href='#'><span>Contact</span></a></li>
+		      </ul>
+		   </li>
+<!--		   ///<li class='last'><a href='#'><span>Contact</span></a></li>-->
+		</ul>
+	    </div> <!--- End of Accesory Category Item. -->
+	     <br>
+	     
+	     <div id='cssmenu'>
+		<ul>
+		    <li class='active'><a href='#'><span>Accesories<img src="img/acc.png" width="35px" height="30px" style=" float: right; "></span></a></li>
+		   <li class='has-sub'><a href='#'><span>Products</span></a>
+		      <ul>
+			 <li><a href='#'><span>Product 1</span></a></li>
+			 <li><a href='#'><span>Product 2</span></a></li>
+			 <li class='last'><a href='#'><span>Product 3</span></a></li>
+		      </ul>
+		   </li>
+		   <li class='has-sub'><a href='#'><span>About</span></a>
+		      <ul>
+			 <li><a href='#'><span>Company</span></a></li>
+			 <li class='last'><a href='#'><span>Contact</span></a></li>
+		      </ul>
+		   </li>
+<!--		   ///<li class='last'><a href='#'><span>Contact</span></a></li>-->
+		</ul>
+	    </div> <!--- End of Accesory Category Item. -->
+	</div> <!--- End of Block LeftSide. -->
+	
+    
+	<div id ="content">
+	    <div id="home">
+		
 	    </div>
+	    <div id="Pro-header">
+		<p id="Pro-title">Title</p>
+	    </div>
+	   <div id="Pro-show"> <!-- Box for show Item. -->
+		<div Class="Pro-Box"> <!-- Box for Item. -->
+		    <div class="imagBox"><!-- Box for Image. -->
+			
+			<img src="img/car.jpg" width="190px" height="170px" >
+			
+		    </div><!-- End of Image. -->
+		    
+		    
+		    <div class="ImagBox-Dec"><!-- Box for Image Description. -->
+			<p> Model: 1201-aaa199</p>
+			<p>color: blue.</p>
+			<p>years: 2012.</p>
+			<p> Price: 120000$</p>
+			
+			
+		    </div><!-- End of Box for Image Description. -->
+		    
+		    <div class="Pro-btnView"><!-- Box for for Button View. -->
+			
+			<input class="mybtn" name="btnview" type="button" value="View Detail">
+		    </div><!-- End of Box for Button View. -->
+			 
+		</div> <!-- End of Box for Item. -->
+	   </div> <!-- End of Box show Item. -->
+ 	 </div>
+	
     
-    
+
     <div id="footer">
 		<div id="footerMenu">
 		    <div id="footer-nav">
 			<ul>
 			    <li><a href="#">Home</a>
+			    <li> | </li>
 			    <li><a href="#">About us</a>
+				<li> | </li>
 			    <li><a href="#">Contact</a>
+				<li> | </li>
 			    <li><a href="#">Home</a>
+				<li> | </li>
 			    <li><a href="#">About us</a>
+				<li> | </li>
 			    <li><a href="#">Contact</a>
+				<li> | </li>
 			    <li><a href="#">Home</a>
+				<li> | </li>
 			    <li><a href="#">About us</a>
+				<li> | </li>
 			    <li><a href="#">Contact</a>
 			</ul>
 		    </div>
